b04e160bb43390c5eed0672db1333f43ade5070a
Stephen Mangum <stephenmangum01@gmail.com>
2025-09-03 08:02:19 -0500

    Initial commit

---
+++ Lists_Mangum_Stephen/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lists_Mangum_Stephen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
