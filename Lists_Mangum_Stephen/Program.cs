7e833a71efa296bb84a7f648646862795aaebd4d
Stephen Mangum <stephenmangum01@gmail.com>
2025-09-04 08:50:49 -0500

    Add game list operations and output to Program.cs
    Expanded Program.cs to include displaying, adding, removing, sorting, and copying games in the list. Added output statements to show changes in the list and demonstrate various List methods.

--- Lists_Mangum_Stephen/Program.cs
+++ Lists_Mangum_Stephen/Program.cs
@@ -15,6 +15,68 @@ namespace Lists_Mangum_Stephen
                 "Fifa 22",
                 "Minecraft",
             };
-        }
-    }
+
+            string[] otherGames = new string[]
+            {
+                "Tiny Tina's Wonderlands",
+                "Farcry 3|"
+            };
+
+            foreach (string game in games)
+            {
+                Console.WriteLine(game);
+            }
+
+            Console.WriteLine($"Games in list: {games.Count}");
+
+            games.AddRange(otherGames);
+
+            Console.WriteLine($"\n Games in list: {games.Count}");
+
+            if (games.Contains("Halo"))
+            {
+                Console.WriteLine("\nMASTER CHIEF BEATS STEVE!!");
+            }
+            else
+            {
+                games.Add("Halo");
+            }
+            int myInt = 6;
+
+            if (myInt < games.Count)
+            {
+                games.RemoveAt(myInt);
+            }
+            else
+            {
+                Console.WriteLine("\nGame not found!");
+            }
+
+            Console.WriteLine("\nAll games in the list:");
+            foreach (string game in games)
+            {
+                Console.WriteLine(game);
+            }
+
+            games.Sort();
+
+            Console.WriteLine("\nSorted games list:");
+            foreach (string game in games)
+            {
+                Console.WriteLine(game);
+            }
+
+            string[] newList = new string[games.Count];
+
+            games.CopyTo(newList);
+
+            games.Clear();
+
+            Console.WriteLine("\nNew List:");
+            foreach (string game in newList)
+            {
+                Console.WriteLine(game);
+            }
+        } 
+    } 
 }
